46b6041d1b83fc6260156a5ed7291357578f3ed5
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-11-29 16:15:41 +0000

    add the meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,46 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '5c-ui',
+    title: process.env.TITLE,
     htmlAttrs: {
       lang: 'ar'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: process.env.TEXT},
+      { name: 'format-detection', content: 'telephone=yes' },
+      {hid: 'og:title', name: 'og:title', content: process.env.TITLE},
+      { name: 'google-site-verification', content: "ZeiClcfAeb4Ssc-QGjzpPhylmKe2U6-Baks6Fj6AU2A"},
+      {hid: 'keywords', name: 'keywords', content: "5c1, الرياضيات, فصل الرياضيات, موريتانيا"},
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://k.top4top.io/p_25249p2r71.png'
+      },
+      {
+        hid: 'og:image:secure_url',
+        property: 'og:image:secure_url',
+        content: 'https://k.top4top.io/p_25249p2r71.png'
+      },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: process.env.TITLE
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://k.top4top.io/p_25249p2r71.png'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ]
   },
 
@@ -30,7 +58,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -40,5 +69,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  
+  googleAnalytics: {
+    id: 'UA-XXX-X'
   }
+
 }

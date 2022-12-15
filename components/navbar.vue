819cd6d5f8b91cf6c3186a4af729de5875c33b9a
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-12-15 11:55:38 +0000

    fix problem of share

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,7 +4,7 @@
     <!-- right -->
     <div class="flex items-center">
       <!-- icon -->
-      <div class="mx-2" v-on:click="share">
+      <div class="mx-2">
           <img src="~/assets/images/icon.png" alt="icon" class="w-16 h-16">
       </div>
       <!-- title -->
@@ -13,7 +13,7 @@
       </div>
     </div>
     <!-- share button -->
-    <div class="mx-2" v-on="share()">
+    <div class="mx-2" v-on:click="share">
         <img src="~/assets/images/arrow-up.svg" alt="share button" class="w-5 h-5">
     </div>
   </div>
@@ -24,14 +24,11 @@ export default {
     name: "navbar",
     methods: {
       share(){
-        if(navigator.share){
           navigator.share({
             title: process.env.TITLE,
             text: process.env.TEXT,
             url: process.env.URL
-          }).then(() => console.log('Successful share'))
-          .catch((error) => console.log('Error sharing', error));
-        } 
+          });
       }
     },
     data(){

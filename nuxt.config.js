b9e275c27b85e91a27a8881e296bedd6abbc9819
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-12-24 13:28:24 +0000

    add ..

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   router: {
     base: '/',
-    // middleware: "redirectToPage"
+    middleware: "redirectToPage"
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
